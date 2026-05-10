09208f838dacdf552eb29ed27b9936bbf1c531c7
MarcusBeladona <marcusbeladona@gmail.com>
2026-05-10 01:50:52 -0300

    refacft vueimage & vuecontainer

--- app/components/VueContainer.vue
+++ app/components/VueContainer.vue
@@ -18,17 +18,17 @@
 <template>
 	<div :class="[
 		isSpanFull ? 'col-span-full' : 'col-span-8 col-start-3',
-		hasContainer ? 'flex gap-4 flex-col bg-zinc-100 p-4 rounded-3xl border border-white ring ring-zinc-200 vue-container--has-container' : 'flex gap-6'
+		hasContainer ? 'flex flex-col bg-zinc-100 rounded-3xl border border-white ring ring-zinc-200 vue-container--has-container' : 'flex gap-6'
 	]">
-		<span v-if="hasContainer" class="flex gap-2">
-			<Icon name="ph:circle-fill" size="20" class="text-zinc-200" />
-			<Icon name="ph:circle-fill" size="20" class="text-zinc-200" />
-			<Icon name="ph:circle-fill" size="20" class="text-zinc-200" />
+		<span v-if="hasContainer" class="flex gap-2 py-3 px-4">
+			<Icon name="ph:circle-fill" size="16" class="text-zinc-200" />
+			<Icon name="ph:circle-fill" size="16" class="text-zinc-200" />
+			<Icon name="ph:circle-fill" size="16" class="text-zinc-200" />
 		</span>
 		<span class="flex flex-col md:flex-row gap-6">
 			<template v-for="item in items" :key="item._key">
 				<VueCard v-if="item._type === 'card'" :icon="item.icon_name" :title="item.title" :description="item.description" class="flex-1" />
-				<VueImage v-else-if="item._type === 'imageBlock' && item.image?.asset?._ref" :asset-id="item.image.asset._ref" :caption="item.caption" :has-border="Boolean(item.hasBorder)" class="flex-1" />
+				<VueImage v-else-if="item._type === 'imageBlock' && item.image?.asset?._ref" :asset-id="item.image.asset._ref" :caption="item.caption" :has-border="Boolean(item.hasBorder)" class="flex-1 rounded-3xl" />
 			</template>
 		</span>
 	</div>
@@ -38,6 +38,6 @@
 	@reference "@/assets/css/main.css";
 
 	.vue-container--has-container :deep(img) {
-		@apply rounded-2xl;
+		@apply rounded-3xl;
 	}
 </style>
--- app/components/VueImage.vue
+++ app/components/VueImage.vue
@@ -30,26 +30,20 @@
 
 <template>
 	<figure :class="isSpanFull ? 'col-span-full' : 'col-span-8 col-start-3'" class="flex flex-col gap-2">
-		<section
-		:class="hasContainer ? 'flex gap-4 flex-col bg-zinc-100 p-4 rounded-3xl border border-white ring ring-zinc-200' : ''"
-		class="rounded-3xl">
-			<span v-if="hasContainer" class="flex gap-2">
-				<Icon name="ph:circle-fill" size="20" class="text-zinc-200" />
-				<Icon name="ph:circle-fill" size="20" class="text-zinc-200" />
-				<Icon name="ph:circle-fill" size="20" class="text-zinc-200" />
+		<section :class="hasContainer ? 'flex flex-col bg-zinc-100 rounded-3xl border border-white ring ring-zinc-200' : ''">
+			<span v-if="hasContainer" class="flex gap-2 py-3 px-4">
+				<Icon name="ph:circle-fill" size="16" class="text-zinc-200" />
+				<Icon name="ph:circle-fill" size="16" class="text-zinc-200" />
+				<Icon name="ph:circle-fill" size="16" class="text-zinc-200" />
 			</span>
-			<SanityImage @click="toggleImageView" :asset-id="assetId" :class="[
-				hasBorder ? 'border border-white ring ring-zinc-200' : '',
-				hasContainer ? 'rounded-2xl' : 'rounded-3xl'
-			]" class="hover:scale-102 transition cursor-zoom-in" />
+			<SanityImage @click="toggleImageView" :asset-id="assetId" :class="hasBorder ? 'border border-zinc-200 ring ring-white' : ''
+				" class="hover:scale-102 transition cursor-zoom-in rounded-3xl" />
 		</section>
 		<figcaption v-if="caption" class="text-center italic">{{ caption }}</figcaption>
 		<!-- Image Fullscreen Modal -->
 		<Transition name="fade" appear>
-			<div v-if="imageView" @click="toggleImageView"
-			class="z-50 fixed inset-0 flex justify-center items-center bg-black/66 p-6 w-full h-full transition duration-200 cursor-zoom-out">
-				<SanityImage :asset-id="assetId"
-				class="rounded-3xl w-fit max-w-full h-full max-h-full object-scale-down animate-grow" />
+			<div v-if="imageView" @click="toggleImageView" class="z-50 fixed inset-0 flex justify-center items-center bg-black/66 p-6 w-full h-full transition duration-200 cursor-zoom-out">
+				<SanityImage :asset-id="assetId" class="h-full w-fit object-scale-down animate-grow rounded-3xl" />
 			</div>
 		</Transition>
 	</figure>
@@ -60,7 +54,7 @@
 
 	.fade-enter-active,
 	.fade-leave-active {
-		transition: all 300ms ease;
+		transition: all 300ms ease-in-out;
 	}
 
 	.fade-enter-from,
