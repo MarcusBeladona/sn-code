d5460ef6e6c75bad533ebee3eb4007486820e408
MarcusBeladona <marcusbeladona@gmail.com>
2026-07-05 07:30:56 -0300

    grid fix

--- app/components/block/GridBlock.vue
+++ app/components/block/GridBlock.vue
@@ -8,8 +8,8 @@
 	<span class="flex flex-col md:flex-row gap-6 justify-start items-start" :class="data.fullWidth ? 'w-full' : 'w-178'">
 		<template v-for="item in data.items">
 			<article class="w-full">
-				<FigureBlock v-if="item._type == 'figure'" :data="item" class="w-full" />
-				<VideoBlock v-else-if="item._type == 'video'" :data="item" class="w-full" />
+				<FigureBlock v-if="item._type == 'figureBlock'" :data="item" class="w-full" />
+				<VideoBlock v-else-if="item._type == 'videoBlock'" :data="item" class="w-full" />
 				<CardBlock v-else-if="item._type == 'cardBlock'" :data="item" class="w-full" />
 			</article>
 		</template>
